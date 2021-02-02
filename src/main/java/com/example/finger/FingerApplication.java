ca64fbd9d39a3b6db59cfe5a79f86669b2a462ec
losun1 <sswmjoy@gmail.com>
2021-02-02 11:04:43 +0800

    change pwd

---
+++ src/main/java/com/example/finger/FingerApplication.java
@@ -0,0 +1,13 @@
+package com.example.finger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingerApplication.class, args);
+    }
+
+}
